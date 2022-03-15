54678917f541b7bdd111c1a77300c0c29b6c6fcf
HYONEE <hellohk07@gmail.com>
2022-03-15 18:03:53 +0900

    practice starbucks page - Vanilla JS

---
+++ js/main.js
@@ -0,0 +1,165 @@
+//* 검색 요소 클릭 이벤트
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input'); 
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+//* Scroll 다운으로 배지 숨기기/보이기
+const bagdeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // bagdeEl.style.display = 'none';
+    // gsap.to(요소, 지속시간, 옵션(객체형태))
+    gsap.to(bagdeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+
+    //! 버튼 보이기
+    gsap.to(toTopEl, .2, {
+      x: 0
+    });
+  } else {
+    // 배지 보이기
+    // bagdeEl.style.display = 'block';
+    gsap.to(bagdeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+
+    //! 버튼 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간)
+
+// * Scroll to Top 
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0  // gsap 플러그인 ScollToPlugin
+  })
+})
+
+//* fade-in 효과주기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션(객체형태))
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 0.7 -> 1.4 -> 2.1 -> 2.7초
+    opacity: 1
+  });
+});
+
+//* Swiper 슬라이드 (공지사항)
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+//* Swiper 슬라이드 (프로모션)
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3,  // 한 번에 보여줄 슬라이스 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true,  // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000, // 5s
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+//* Slide (awards)
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next',
+  }
+})
+
+//* Toggle on/off
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    // 숨기기
+    promotionEl.classList.add('hide');
+  } else {
+    // 보이기
+    promotionEl.classList.remove('hide');
+  }
+});
+
+//* Youtube Icons floating
+function floatingObjcet(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션) https://greensock.com/docs/v3/GSAP/gsap.to()
+  gsap.to(selector, 
+    random(1.5, 2.5), 
+    {
+      y: size,
+      repeat: -1,  //* 무한반복
+      yoyo: true, // 재생된 애니메이션을 다시 뒤로 재생 (위 아래로 둥둥)
+      ease: Power1.easeInOut,  // easing 제어: https://greensock.com/docs/v2/Easing
+      delay: random(0, delay)
+    }
+  );
+}
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+floatingObjcet('.floating1', 1, 15);
+floatingObjcet('.floating2', .5, 15);
+floatingObjcet('.floating3', 1.5, 20);
+
+// * Scroll Magic
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,  // 보여짐 여부를 감시하는 요소 지정
+      triggerHook: .8,  // 뷰포트(0 ~ 1) 0.8 지점 (요소가 걸리는 지점)
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+})
+
+// * 해당 년 계산하기
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
